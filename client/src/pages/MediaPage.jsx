f447cb419915408a3c17add00e72fe1eee96a195
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-01-27 21:59:47 -0500

    added page to display songs

--- client/src/pages/MediaPage.jsx
+++ client/src/pages/MediaPage.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from "react"
+import postMalone from '../assets/postMalone.jpg'
+import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 function MediaPage() {
+    const [artistState, setArtistState] = useState([])
+
     const location = useLocation()
     const params = new URLSearchParams(location.search)
 
     const firstName = params.get('firstName')
     const lastName = params.get('lastName')
+
 
     useEffect(() => {
         const getData = async () => {
@@ -18,15 +22,35 @@ function MediaPage() {
                 }
 
                 const data = await response.json()
-                console.log(data)
+
+                const filteredData = data.results.filter((song, index, arr) =>
+                    arr.findIndex(item => item.collectionName === song.collectionName && item.artworkUrl100 === song.artworkUrl100) === index
+                );
+
+                const updateArtistState = filteredData.map((song) => ({
+                    collectionName: song.collectionName,
+                    imgUrl: song.artworkUrl100
+                }));
+
+                setArtistState(updateArtistState)
+                console.log(updateArtistState)
             } catch (error) {
                 console.error(error)
             }
+
         }
         getData()
-    }, [])
+    }, [firstName, lastName])
     return (
-        <h1 style={{ color: "white" }}>Category</h1>
+        <section className="song-container">
+            {artistState.map((song, index) => (
+                <article className='song-article'>
+                    <p>{song.collectionName}</p>
+                    <img src={song.imgUrl} alt="Post Malone" className='song-img' />
+                    <button className='save-btn'>Save</button>
+                </article>
+            ))}
+        </section>
     )
 }
 
